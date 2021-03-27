d630554498cef8fc2287a4bad88e62f5e7a316f9
bon_92 <mirai1735@icloud.com>
2021-03-27 17:32:12 +0900

    テーブルとコントローラ作成

---
+++ database/migrations/2021_03_27_171241_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->string('image_title', 100);
+            $table->string('image_path', 100);
+            $table->string('receiptimage_path', 100);
+            $table->text('weather');
+            $table->integer('plice')
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
